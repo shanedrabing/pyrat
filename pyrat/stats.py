d0ea571978497866a9a5241368a9d63da6961376
Shane Drabing <shane.drabing@gmail.com>
2021-07-25 13:28:22 -0600

    var and cov

--- pyrat/stats.py
+++ pyrat/stats.py
@@ -1,14 +1,14 @@
 __all__ = [
     # "cor",
-    # "cov",
-    # "dev",
+    "cov",
+    "dev",
     # "lm",
     # "na_omit",
     # "predict",
     # "rmse",
     # "sd",
-    # "ss",
-    # "var",
+    "ss",
+    "var",
 ]
 
 
@@ -16,9 +16,38 @@ __all__ = [
 
 
 import statistics
-from pyrat.base import mean, sqrt, is_na, vector
+from pyrat.base import mean, sqrt, is_na, vector, c, NA
 from pyrat.closure import inv
 
 
 # FUNCTIONS
 
+
+def ss(x):
+    return sum(x ** 2)
+
+
+def dev(x, f=mean):
+    return x - f(x)
+
+
+def var(x, y=None):
+    if y is not None:
+        return cov(x, y)
+    if not isinstance(x, vector):
+        x = c(x)
+    if len(x) <= 1:
+        return NA
+    return ss(dev(x)) / (len(x) - 1)
+
+
+def cov(x, y=None):
+    if y is None:
+        return var(x)
+    if not isinstance(x, vector):
+        x = c(x)
+    if not isinstance(y, vector):
+        y = c(y)
+    if len(x) != len(y):
+        raise ValueError("vector lengths unequal")
+    return sum(dev(x) * dev(y)) / (len(x) - 1)
